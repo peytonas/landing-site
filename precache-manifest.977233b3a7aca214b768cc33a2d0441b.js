6952dbf910663d35ec88ca2c40d9911c2fdfd02f
Peyton Sonnefeld <peyton.sonnefeld@gmail.com>
2020-05-11 14:33:28 -0600

    Updates

---
+++ precache-manifest.977233b3a7aca214b768cc33a2d0441b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce9e8762a77805ce526094b7952d9560",
+    "url": "/landing-site/index.html"
+  },
+  {
+    "revision": "73515250b3a19200457d",
+    "url": "/landing-site/static/css/main.05a70f93.chunk.css"
+  },
+  {
+    "revision": "0220157321d58ea7710e",
+    "url": "/landing-site/static/js/2.0ab74bc0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/landing-site/static/js/2.0ab74bc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73515250b3a19200457d",
+    "url": "/landing-site/static/js/main.9319b9f6.chunk.js"
+  },
+  {
+    "revision": "1724d00501160b8efcea",
+    "url": "/landing-site/static/js/runtime-main.916e152a.js"
+  }
+]);
